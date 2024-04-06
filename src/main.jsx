17ea51cdf00515cb668425d75e94f08b125fe80d
Dana Fleming <danagfleming@gmail.com>
2024-04-06 11:46:03 -0700

    L2: Complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import SunAndMoon from "./SunAndMoon/sunandmoon.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>Dana Fleming</React.StrictMode>
+    <React.StrictMode>
+        <SunAndMoon />
+    </React.StrictMode>
 );
