2a15a1deac98d67bdb4726b46bdea7c78781ba8f
Dana Fleming <danagfleming@gmail.com>
2024-04-15 19:46:28 -0700

    L3 Complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import SunAndMoon from "./SunAndMoon/sunandmoon.jsx";
+import Routes from "./Pages/Routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <SunAndMoon />
+        <Routes />
     </React.StrictMode>
 );
